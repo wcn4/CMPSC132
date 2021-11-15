58d3dceeb6266f8369bb37e5be1a66e4c31d20cd
wcn4 <bill.neubauer.4@gmail.com>
2021-11-15 00:31:13 -0500

    Started Working on HW5.py, wrote a few methods for CacheList, did not finish or test.

---
+++ HW5.py
@@ -0,0 +1,315 @@
+# HW5
+# Due Date: 11/19/2021, 11:59PM
+# REMINDER: 
+#       The work in this assignment must be your own original work and must be completed alone.
+
+
+class Node:
+    def __init__(self, content):
+        self.value = content
+        self.next = None
+
+    def __str__(self):
+        return ('CONTENT:{}\n'.format(self.value))
+
+    __repr__=__str__
+
+
+class ContentItem:
+    '''
+        >>> content1 = ContentItem(1000, 10, "Content-Type: 0", "0xA")
+        >>> content2 = ContentItem(1004, 50, "Content-Type: 1", "110010")
+        >>> content3 = ContentItem(1005, 18, "Content-Type: 2", "<html><p>'CMPSC132'</p></html>")
+        >>> content4 = ContentItem(1005, 18, "another header", "111110")
+        >>> hash(content1)
+        0
+        >>> hash(content2)
+        1
+        >>> hash(content3)
+        2
+        >>> hash(content4)
+        1
+    '''
+    def __init__(self, cid, size, header, content):
+        self.cid = cid
+        self.size = size
+        self.header = header
+        self.content = content
+
+    def __str__(self):
+        return f'CONTENT ID: {self.cid} SIZE: {self.size} HEADER: {self.header} CONTENT: {self.content}'
+
+    __repr__=__str__
+
+    def __eq__(self, other):
+        if isinstance(other, ContentItem):
+            return self.cid == other.cid and self.size == other.size and self.header == other.header and self.content == other.content
+        return False
+
+    #Computes the Hash value based off the sum of all ASCII characters
+    #in the header mod 3
+    def __hash__(self):
+        total = 0
+        for char in self.header:
+            total += ord(char)
+        return total%3
+
+
+class CacheList:
+    ''' 
+        # An extended version available on Canvas. Make sure you pass this doctest first before running the extended version
+
+        >>> content1 = ContentItem(1000, 10, "Content-Type: 0", "0xA")
+        >>> content2 = ContentItem(1004, 50, "Content-Type: 1", "110010")
+        >>> content3 = ContentItem(1005, 180, "Content-Type: 2", "<html><p>'CMPSC132'</p></html>")
+        >>> content4 = ContentItem(1006, 18, "another header", "111110")
+        >>> content5 = ContentItem(1008, 2, "items", "11x1110")
+        >>> lst=CacheList(200)
+        >>> lst
+        REMAINING SPACE:200
+        ITEMS:0
+        LIST:
+        <BLANKLINE>
+        >>> lst.put(content1, 'mru')
+        'INSERTED: CONTENT ID: 1000 SIZE: 10 HEADER: Content-Type: 0 CONTENT: 0xA'
+        >>> lst.put(content2, 'lru')
+        'INSERTED: CONTENT ID: 1004 SIZE: 50 HEADER: Content-Type: 1 CONTENT: 110010'
+        >>> lst.put(content4, 'mru')
+        'INSERTED: CONTENT ID: 1006 SIZE: 18 HEADER: another header CONTENT: 111110'
+        >>> lst.put(content5, 'mru')
+        'INSERTED: CONTENT ID: 1008 SIZE: 2 HEADER: items CONTENT: 11x1110'
+        >>> lst.put(content3, 'lru')
+        "INSERTED: CONTENT ID: 1005 SIZE: 180 HEADER: Content-Type: 2 CONTENT: <html><p>'CMPSC132'</p></html>"
+        >>> lst.put(content1, 'mru')
+        'INSERTED: CONTENT ID: 1000 SIZE: 10 HEADER: Content-Type: 0 CONTENT: 0xA'
+        >>> 1006 in lst
+        True
+        >>> contentExtra = ContentItem(1034, 2, "items", "other content")
+        >>> lst.update(1008, contentExtra)
+        'UPDATED: CONTENT ID: 1034 SIZE: 2 HEADER: items CONTENT: other content'
+        >>> lst
+        REMAINING SPACE:170
+        ITEMS:3
+        LIST:
+        [CONTENT ID: 1034 SIZE: 2 HEADER: items CONTENT: other content]
+        [CONTENT ID: 1006 SIZE: 18 HEADER: another header CONTENT: 111110]
+        [CONTENT ID: 1000 SIZE: 10 HEADER: Content-Type: 0 CONTENT: 0xA]
+        <BLANKLINE>
+        >>> lst.clear()
+        'Cleared cache!'
+        >>> lst
+        REMAINING SPACE:200
+        ITEMS:0
+        LIST:
+        <BLANKLINE>
+    '''
+    def __init__(self, size):
+        self.head = None
+        self.maxSize = size
+        self.remainingSpace = size
+        self.numItems = 0
+
+    def __str__(self):
+        listString = ""
+        current = self.head
+        while current is not None:
+            listString += "[" + str(current.value) + "]\n"
+            current = current.next
+        return 'REMAINING SPACE:{}\nITEMS:{}\nLIST:\n{}'.format(self.remainingSpace, self.numItems, listString)  
+
+    __repr__=__str__
+
+    def __len__(self):
+        return self.numItems
+   
+   #Adds nodes to beginning of the list and evicts items
+   #as necessary to free up space
+    def put(self, content, evictionPolicy):
+        #If less than max size, do not evict
+        #If exceeds max size, evict according to eviction policy
+        #If contend iD exists in list before insertion, move content to the beginning of list
+        pass
+    
+    #Finds an item from list by id
+    #Moves item to the front of the list if found
+    #Overload of the in function
+    def __contains__(self, cid):
+        #If size == 0, return False
+        #If size == 1, return self.head.cid == cid
+        if (self.numItems <= 1):
+            return self.head and self.head.cid == cid
+        
+        prevItem = None
+        currItem = self.head
+        #While the item is not found, iterate to the next item
+        while (currItem and currItem.cid != cid):
+            prevItem = currItem
+            currItem = currItem.next
+
+        #If the item was not found, return False
+        if (currItem == None):
+            return False
+        #If the item is found, moves the item to the front
+        prevItem.next = currItem.next
+        currItem.next = self.head
+        self.head = currItem
+        return True
+
+
+    def update(self, cid, content):
+        if (self.numItems <= 1):
+            #If there is an element, and that element matches the id and the new change 
+            #does not exceed the max size, update item
+            if (self.head and self.head.cid == cid and (content.size - self.head.size) <= self.maxSize):
+                self.head = content
+                return f'UPDATED: {content}'
+            else:
+                return 'Cache miss!'
+        prevItem = None
+        currItem = self.head
+        #While the item is not found, iternate to the next item
+        while (currItem and currItem.cid != cid):
+            prevItem = currItem
+            currItem = currItem.next
+        #If the item was not found:
+        if (currItem == None):
+            return 'Cache miss!'
+        #If the item was found but the new size exceeds max size
+        if ((content.size - currItem.size) + self.size) > self.maxSize:
+            return 'Cache miss!'
+        #Delete the item found and move the updated item to the front
+        prevItem.next = currItem.next
+        content.next = self.head
+        self.head = content
+        return f'UPDATED: {content}'
+
+    #Removes most recently used element (first element)
+    def mruEvict(self):
+        if (self.numItems <= 1):
+            self.head = None
+            self.remainingSpace = self.maxSize
+            self.numItems = 0
+            return
+
+        self.remainingSpace += self.head.size 
+        self.head = self.head.next
+        self.numItems -= 1
+    
+    #Removes least recently used element (last element)
+    #Takes O(n) time to execute as it has to get to the end of the list
+    def lruEvict(self):
+        if (self.numItems <= 1):
+            self.head = None
+            self.remainingSpace = self.maxSize
+            self.numItems = 0
+            return
+        temp = self.head
+        while (temp.next.next != None):
+            temp = temp.next
+
+        self.remainingSpace += temp.next.size
+        temp.next = None
+        self.numItems -= 1
+
+    #Removes all items from the list
+    def clear(self):
+        self.head = None
+        self.remainingSpace = self.maxSize
+        self.numItems = 0
+
+class Cache:
+    """
+        # An extended version available on Canvas. Make sure you pass this doctest first before running the extended version
+
+        >>> cache = Cache()
+        >>> content1 = ContentItem(1000, 10, "Content-Type: 0", "0xA")
+        >>> content2 = ContentItem(1003, 13, "Content-Type: 0", "0xD")
+        >>> content3 = ContentItem(1008, 242, "Content-Type: 0", "0xF2")
+
+        >>> content4 = ContentItem(1004, 50, "Content-Type: 1", "110010")
+        >>> content5 = ContentItem(1001, 51, "Content-Type: 1", "110011")
+        >>> content6 = ContentItem(1007, 155, "Content-Type: 1", "10011011")
+
+        >>> content7 = ContentItem(1005, 18, "Content-Type: 2", "<html><p>'CMPSC132'</p></html>")
+        >>> content8 = ContentItem(1002, 14, "Content-Type: 2", "<html><h2>'PSU'</h2></html>")
+        >>> content9 = ContentItem(1006, 170, "Content-Type: 2", "<html><button>'Click Me'</button></html>")
+
+        >>> cache.insert(content1, 'lru')
+        'INSERTED: CONTENT ID: 1000 SIZE: 10 HEADER: Content-Type: 0 CONTENT: 0xA'
+        >>> cache.insert(content2, 'lru')
+        'INSERTED: CONTENT ID: 1003 SIZE: 13 HEADER: Content-Type: 0 CONTENT: 0xD'
+        >>> cache.insert(content3, 'lru')
+        'Insertion not allowed'
+
+        >>> cache.insert(content4, 'lru')
+        'INSERTED: CONTENT ID: 1004 SIZE: 50 HEADER: Content-Type: 1 CONTENT: 110010'
+        >>> cache.insert(content5, 'lru')
+        'INSERTED: CONTENT ID: 1001 SIZE: 51 HEADER: Content-Type: 1 CONTENT: 110011'
+        >>> cache.insert(content6, 'lru')
+        'INSERTED: CONTENT ID: 1007 SIZE: 155 HEADER: Content-Type: 1 CONTENT: 10011011'
+
+        >>> cache.insert(content7, 'lru')
+        "INSERTED: CONTENT ID: 1005 SIZE: 18 HEADER: Content-Type: 2 CONTENT: <html><p>'CMPSC132'</p></html>"
+        >>> cache.insert(content8, 'lru')
+        "INSERTED: CONTENT ID: 1002 SIZE: 14 HEADER: Content-Type: 2 CONTENT: <html><h2>'PSU'</h2></html>"
+        >>> cache.insert(content9, 'lru')
+        "INSERTED: CONTENT ID: 1006 SIZE: 170 HEADER: Content-Type: 2 CONTENT: <html><button>'Click Me'</button></html>"
+        >>> cache
+        L1 CACHE:
+        REMAINING SPACE:177
+        ITEMS:2
+        LIST:
+        [CONTENT ID: 1003 SIZE: 13 HEADER: Content-Type: 0 CONTENT: 0xD]
+        [CONTENT ID: 1000 SIZE: 10 HEADER: Content-Type: 0 CONTENT: 0xA]
+        <BLANKLINE>
+        L2 CACHE:
+        REMAINING SPACE:45
+        ITEMS:1
+        LIST:
+        [CONTENT ID: 1007 SIZE: 155 HEADER: Content-Type: 1 CONTENT: 10011011]
+        <BLANKLINE>
+        L3 CACHE:
+        REMAINING SPACE:16
+        ITEMS:2
+        LIST:
+        [CONTENT ID: 1006 SIZE: 170 HEADER: Content-Type: 2 CONTENT: <html><button>'Click Me'</button></html>]
+        [CONTENT ID: 1002 SIZE: 14 HEADER: Content-Type: 2 CONTENT: <html><h2>'PSU'</h2></html>]
+        <BLANKLINE>
+        <BLANKLINE>
+    """
+
+    def __init__(self):
+        self.hierarchy = [CacheList(200), CacheList(200), CacheList(200)]
+        self.size = 3
+    
+    def __str__(self):
+        return ('L1 CACHE:\n{}\nL2 CACHE:\n{}\nL3 CACHE:\n{}\n'.format(self.hierarchy[0], self.hierarchy[1], self.hierarchy[2]))
+    
+    __repr__=__str__
+
+
+    def clear(self):
+        for item in self.hierarchy:
+            item.clear()
+        return 'Cache cleared!'
+
+    
+    def insert(self, content, evictionPolicy):
+        # YOUR CODE STARTS HERE
+        pass
+
+
+    def __getitem__(self, content):
+        # YOUR CODE STARTS HERE
+        pass
+
+
+
+    def updateContent(self, content):
+        # YOUR CODE STARTS HERE
+        pass
+
+
+if __name__=='__main__': 
+    import doctest
+    doctest.run_docstring_examples(ContentItem, globals(), name='HW5', verbose=True)
